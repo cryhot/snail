366a3d41cee90f9a371cc52d62909c3025d48da7
Jean-Raphaël GAGLIONE <jr.gaglione@yahoo.fr>
2017-02-12 16:30:00 +0100

    snail.sh pass the shellcheck test

--- snail.sh
+++ snail.sh
@@ -10,20 +10,21 @@ function mill() {
         esac
         shift
     done
+    local cwd
     while :; do
         clear
-        local cwd="$(pwd)"
+        cwd="$(pwd)"
         [ "$cwd" = "$HOME" ] && cwd="~" || cwd=$(basename $cwd)
         echo -ne "\033[01;31mmill\033[00m:\033[01;34m${cwd}\033[00m$ "
         echo "$@"
-        eval $@
+        eval "$@"
         sleep "$period"
     done
 }
 
 # scale VAR [MIN] [MAX]
 function scale() {
-    echo dummy | read "$1" 2>/dev/null || {
+    echo dummy | read -r "$1" 2>/dev/null || {
         echo "not a valid identifier" >&2; return 1
     }
     local __prev__=${!1}
@@ -36,32 +37,33 @@ function scale() {
     local __shared__="/dev/shm/scale-$$-$1-$RANDOM"
     echo "$__val__" > "$__shared__"
     ({ # pull values while file exists (drop values when too slow)
-        local oldval="not $__val__"
-        while [ -n "$__val__" ]; do
-            if [ "$__val__" != "$oldval" ]; then
+        local val="$__val__"
+        local oldval="not $val"
+        while [ -n "$val" ]; do
+            if [ "$val" != "$oldval" ]; then
                 gdb --batch-silent -p "$$" \
-                    -ex "set bind_variable(\"$1\", \"$__val__\", 0)" \
+                    -ex "set bind_variable(\"$1\", \"$val\", 0)" \
                     2>/dev/null # thanks BeniBela
-                oldval="$__val__"
+                oldval="$val"
             else
                 sleep 0.2
             fi
-            __val__=$(cat "$__shared__" 2>/dev/null)
+            val=$(cat "$__shared__" 2>/dev/null)
         done
     }&)
     ({ # push values (could be in foreground if an explicit call to the function with "&" is desired)
-    while [ -n "$__val__" ]; do
-        echo "$__val__" > "$__shared__"
-        read "__val__"
-    done < <(zenity --scale --print-partial "--text=$1=" "--title=Interactive variable modifier" \
-        --value=$__val__ --min-value=$__min__ --max-value=$__max__ 2>/dev/null)
-    rm "$__shared__"
+        while [ -n "$__val__" ]; do
+            echo "$__val__" > "$__shared__"
+            read -r "__val__"
+        done < <(zenity --scale --print-partial "--text=$1=" "--title=Interactive variable modifier" \
+            "--value=$__val__" "--min-value=$__min__" "--max-value=$__max__" 2>/dev/null)
+        rm "$__shared__"
     }&)
 }
 
 # ++ VAR [MAX] [MIN]
 function ++ {
-    echo dummy | read "$1" || {
+    echo dummy | read -r "$1" || {
         echo "not a valid identifier" >&2; return 1
     }
     local __max__=""
@@ -75,25 +77,25 @@ function ++ {
             __min__="$__max__"
             __max__="$__val__"
         fi
-        __val__=${!1-$(($__min__-1))}
+        __val__=${!1-$((__min__-1))}
     else
         __val__=${!1}
     fi
     [ "$__val__" -eq "$__val__" ] 2>/dev/null || {
         echo "\$$1 is NaN" >&2; return 2
     }
-    __val__=$(($__val__+1))
+    __val__=$((__val__+1))
     if [ -n "$__max__" ]; then
         if [ "$__val__" -lt "$__min__" ] || [ "$__val__" -gt "$__max__" ]; then
             __val__=$__min__
         fi
     fi
-    eval $1=$__val__
+    eval "$1=$__val__"
 }
 
 # -- VAR [MAX] [MIN]
 function -- {
-    echo dummy | read "$1" || {
+    echo dummy | read -r "$1" || {
         echo "not a valid identifier" >&2; return 1
     }
     local __max__=""
@@ -107,18 +109,18 @@ function -- {
             __min__="$__max__"
             __max__="$__val__"
         fi
-        __val__=${!1-$(($__max__+1))}
+        __val__=${!1-$((__max__+1))}
     else
         __val__=${!1}
     fi
     [ "$__val__" -eq "$__val__" ] 2>/dev/null || {
         echo "\$$1 is NaN" >&2; return 2
     }
-    __val__=$(($__val__-1))
+    __val__=$((__val__-1))
     if [ -n "$__max__" ]; then
         if [ "$__val__" -lt "$__min__" ] || [ "$__val__" -gt "$__max__" ]; then
             __val__=$__max__
         fi
     fi
-    eval $1=$__val__
+    eval "$1=$__val__"
 }
